776d145dc8cea064e96036a065655aabf133c193
inosine4wobble <inosine@u.washington.edu>
2024-05-21 14:22:27 -0700

    Add files via upload

---
+++ Assignment06_EstherAller.sql
@@ -0,0 +1,428 @@
+--*************************************************************************--
+-- Title: Assignment06
+-- Author: EstherAller
+-- Desc: This file demonstrates how to use Views
+-- Change Log: When,Who,What
+-- 2017-01-01,EstherAller,Created File
+--**************************************************************************--
+Begin Try
+	Use Master;
+	If Exists(Select Name From SysDatabases Where Name = 'Assignment06DB_EstherAller')
+	 Begin 
+	  Alter Database [Assignment06DB_EstherAller] set Single_user With Rollback Immediate;
+	  Drop Database Assignment06DB_EstherAller;
+	 End
+	Create Database Assignment06DB_EstherAller;
+End Try
+Begin Catch
+	Print Error_Number();
+End Catch
+go
+Use Assignment06DB_EstherAller;
+
+-- Create Tables (Module 01)-- 
+Create Table Categories
+([CategoryID] [int] IDENTITY(1,1) NOT NULL 
+,[CategoryName] [nvarchar](100) NOT NULL
+);
+go
+
+Create Table Products
+([ProductID] [int] IDENTITY(1,1) NOT NULL 
+,[ProductName] [nvarchar](100) NOT NULL 
+,[CategoryID] [int] NULL  
+,[UnitPrice] [mOney] NOT NULL
+);
+go
+
+Create Table Employees -- New Table
+([EmployeeID] [int] IDENTITY(1,1) NOT NULL 
+,[EmployeeFirstName] [nvarchar](100) NOT NULL
+,[EmployeeLastName] [nvarchar](100) NOT NULL 
+,[ManagerID] [int] NULL  
+);
+go
+
+Create Table Inventories
+([InventoryID] [int] IDENTITY(1,1) NOT NULL
+,[InventoryDate] [Date] NOT NULL
+,[EmployeeID] [int] NOT NULL -- New Column
+,[ProductID] [int] NOT NULL
+,[Count] [int] NOT NULL
+);
+go
+
+-- Add Constraints (Module 02) -- 
+Begin  -- Categories
+	Alter Table Categories 
+	 Add Constraint pkCategories 
+	  Primary Key (CategoryId);
+
+	Alter Table Categories 
+	 Add Constraint ukCategories 
+	  Unique (CategoryName);
+End
+go 
+
+Begin -- Products
+	Alter Table Products 
+	 Add Constraint pkProducts 
+	  Primary Key (ProductId);
+
+	Alter Table Products 
+	 Add Constraint ukProducts 
+	  Unique (ProductName);
+
+	Alter Table Products 
+	 Add Constraint fkProductsToCategories 
+	  Foreign Key (CategoryId) References Categories(CategoryId);
+
+	Alter Table Products 
+	 Add Constraint ckProductUnitPriceZeroOrHigher 
+	  Check (UnitPrice >= 0);
+End
+go
+
+Begin -- Employees
+	Alter Table Employees
+	 Add Constraint pkEmployees 
+	  Primary Key (EmployeeId);
+
+	Alter Table Employees 
+	 Add Constraint fkEmployeesToEmployeesManager 
+	  Foreign Key (ManagerId) References Employees(EmployeeId);
+End
+go
+
+Begin -- Inventories
+	Alter Table Inventories 
+	 Add Constraint pkInventories 
+	  Primary Key (InventoryId);
+
+	Alter Table Inventories
+	 Add Constraint dfInventoryDate
+	  Default GetDate() For InventoryDate;
+
+	Alter Table Inventories
+	 Add Constraint fkInventoriesToProducts
+	  Foreign Key (ProductId) References Products(ProductId);
+
+	Alter Table Inventories 
+	 Add Constraint ckInventoryCountZeroOrHigher 
+	  Check ([Count] >= 0);
+
+	Alter Table Inventories
+	 Add Constraint fkInventoriesToEmployees
+	  Foreign Key (EmployeeId) References Employees(EmployeeId);
+End 
+go
+
+-- Adding Data (Module 04) -- 
+Insert Into Categories 
+(CategoryName)
+Select CategoryName 
+ From Northwind.dbo.Categories
+ Order By CategoryID;
+go
+
+Insert Into Products
+(ProductName, CategoryID, UnitPrice)
+Select ProductName,CategoryID, UnitPrice 
+ From Northwind.dbo.Products
+  Order By ProductID;
+go
+
+Insert Into Employees
+(EmployeeFirstName, EmployeeLastName, ManagerID)
+Select E.FirstName, E.LastName, IsNull(E.ReportsTo, E.EmployeeID) 
+ From Northwind.dbo.Employees as E
+  Order By E.EmployeeID;
+go
+
+Insert Into Inventories
+(InventoryDate, EmployeeID, ProductID, [Count])
+Select '20170101' as InventoryDate, 5 as EmployeeID, ProductID, UnitsInStock
+From Northwind.dbo.Products
+UNIOn
+Select '20170201' as InventoryDate, 7 as EmployeeID, ProductID, UnitsInStock + 10 -- Using this is to create a made up value
+From Northwind.dbo.Products
+UNIOn
+Select '20170301' as InventoryDate, 9 as EmployeeID, ProductID, UnitsInStock + 20 -- Using this is to create a made up value
+From Northwind.dbo.Products
+Order By 1, 2
+go
+
+-- Show the Current data in the Categories, Products, and Inventories Tables
+Select * From Categories;
+go
+Select * From Products;
+go
+Select * From Employees;
+go
+Select * From Inventories;
+go
+
+/********************************* Questions and Answers *********************************/
+print 
+'NOTES------------------------------------------------------------------------------------ 
+ 1) You can use any name you like for you views, but be descriptive and consistent
+ 2) You can use your working code from assignment 5 for much of this assignment
+ 3) You must use the BASIC views for each table after they are created in Question 1
+------------------------------------------------------------------------------------------'
+
+-- Question 1 (5% pts): How can you create BASIC views to show data from each table in the database.
+-- NOTES: 1) Do not use a *, list out each column!
+--        2) Create one view per table!
+--		  3) Use SchemaBinding to protect the views from being orphaned!
+GO
+--categories
+CREATE OR ALTER VIEW vCategories 
+WITH SCHEMABINDING
+AS
+	SELECT 
+		 "CategoryID"
+		,"CategoryName"
+	FROM dbo.Categories
+;
+GO
+--SELECT * FROM vCategories;
+
+--products
+CREATE OR ALTER VIEW vProducts 
+WITH SCHEMABINDING
+AS
+	SELECT 
+		 "ProductID"
+		,"ProductName"
+		,"CategoryID"
+		,"UnitPrice"
+	FROM dbo.Products
+;
+GO
+--SELECT * FROM vProducts;
+
+--employees
+CREATE OR ALTER VIEW vEmployees 
+WITH SCHEMABINDING
+AS
+	SELECT 
+		 "EmployeeID"
+		,"EmployeeFirstName"
+		,"EmployeeLastName"
+		,"ManagerID"
+	FROM dbo.Employees
+;
+GO
+--SELECT * FROM vEmployees;
+
+--inventories
+CREATE OR ALTER VIEW vInventories 
+WITH SCHEMABINDING
+AS
+	SELECT 
+		 "InventoryID"
+		,"InventoryDate"
+		,"EmployeeID"
+		,"ProductID"
+		,"Count"
+	FROM dbo.Inventories
+;
+GO
+--SELECT * FROM vInventories
+
+-- Question 2 (5% pts): How can you set permissions, so that the public group CANNOT select data 
+-- from each table, but can select data from each view?
+
+DENY  SELECT ON Categories TO Public;
+GRANT SELECT ON vCategories TO Public;
+
+DENY  SELECT ON Products TO Public;
+GRANT SELECT ON vProducts TO Public;
+
+DENY  SELECT ON Employees TO Public;
+GRANT SELECT ON vEmployees TO Public;
+
+DENY  SELECT ON Inventories TO Public;
+GRANT SELECT ON vInventories TO Public;
+GO
+
+-- Question 3 (10% pts): How can you create a view to show a list of Category and Product names, 
+-- and the price of each product?
+-- Order the result by the Category and Product!
+
+CREATE OR ALTER VIEW vProductsByCategories
+WITH SCHEMABINDING
+AS
+	SELECT TOP 1000000000
+		 "CategoryName"
+		 ,"ProductName"
+		 ,"UnitPrice"
+	FROM dbo.Categories AS C
+	JOIN dbo.Products AS P ON C."CategoryID" = P."CategoryID"
+	ORDER BY "CategoryName", "ProductName"
+GO
+--SELECT * FROM vProductsByCategories
+
+-- Question 4 (10% pts): How can you create a view to show a list of Product names 
+-- and Inventory Counts on each Inventory Date?
+-- Order the results by the Product, Date, and Count!
+
+CREATE OR ALTER VIEW vInventoriesByProductsByDates
+WITH SCHEMABINDING
+AS
+	SELECT TOP 1000000000
+		  "ProductName", P.ProductID
+		 ,"InventoryDate"
+		 ,"Count"
+	FROM dbo.Inventories AS I
+	JOIN dbo.Products AS P ON I."ProductID" = P."ProductID"
+	ORDER BY "ProductName", "InventoryDate", "Count"
+GO
+--SELECT * FROM vInventoriesByProductsByDates;
+--Select * From Inventories WHERE ProductId=17
+
+
+-- Question 5 (10% pts): How can you create a view to show a list of Inventory Dates 
+-- and the Employee that took the count?
+-- Order the results by the Date and return only one row per date!
+
+-- Here is are the rows selected from the view:
+
+-- InventoryDate	EmployeeName
+-- 2017-01-01	    Steven Buchanan
+-- 2017-02-01	    Robert King
+-- 2017-03-01	    Anne Dodsworth
+
+CREATE OR ALTER VIEW vInventoriesByEmployeesByDates
+WITH SCHEMABINDING
+AS
+	SELECT DISTINCT TOP 1000000000
+	--NOTE: if the data is expanded to include more than one employee put date then this view will show multiple rows per date. I lost points for fixing this problem in the last assignment so I'm leaving this as is
+	"InventoryDate"
+	,CONCAT("EmployeeFirstName",' ',"EmployeeLastName") AS "EmployeeName"
+	FROM dbo.Inventories AS I
+	JOIN dbo.Employees AS E ON E."EmployeeID" = I."EmployeeID"
+	ORDER BY "InventoryDate"
+GO
+--SELECT * FROM vInventoriesByEmployeesByDates
+
+-- Question 6 (10% pts): How can you create a view show a list of Categories, Products, 
+-- and the Inventory Date and Count of each product?
+-- Order the results by the Category, Product, Date, and Count!
+
+CREATE OR ALTER VIEW vInventoriesByProductsByCategories
+WITH SCHEMABINDING
+AS
+	SELECT TOP 1000000000
+		 "CategoryName"
+		,"ProductName"
+		,"InventoryDate"
+		,"Count"
+	FROM dbo.Categories AS C
+	JOIN dbo.Products AS P ON P."CategoryID" = C."CategoryID"
+	JOIN dbo.Inventories AS I ON I."ProductID" = P."ProductID"
+	ORDER BY "CategoryName", "ProductName", "InventoryDate", "Count"
+GO
+--SELECT * FROM vInventoriesByProductsByCategories
+
+-- Question 7 (10% pts): How can you create a view to show a list of Categories, Products, 
+-- the Inventory Date and Count of each product, and the EMPLOYEE who took the count?
+-- Order the results by the Inventory Date, Category, Product and Employee!
+
+CREATE OR ALTER VIEW vInventoriesByProductsByEmployees
+WITH SCHEMABINDING
+AS
+	SELECT TOP 1000000000
+		 "CategoryName"
+		,"ProductName"
+		,"InventoryDate"
+		,"Count"
+		,CONCAT("EmployeeFirstName",' ',"EmployeeLastName") AS "EmployeeName"
+	FROM dbo.Categories AS C
+	JOIN dbo.Products AS P ON P."CategoryID" = C."CategoryID"
+	JOIN dbo.Inventories AS I ON I."ProductID" = P."ProductID"
+	JOIN dbo.Employees AS E ON I."EmployeeID" = E."EmployeeID"
+	ORDER BY "CategoryName", "ProductName", "InventoryDate", "Count","EmployeeName"
+GO
+
+--SELECT * FROM vInventoriesByProductsByEmployees
+
+-- Question 8 (10% pts): How can you create a view to show a list of Categories, Products, 
+-- the Inventory Date and Count of each product, and the Employee who took the count
+-- for the Products 'Chai' and 'Chang'? 
+
+CREATE OR ALTER VIEW vInventoriesForChaiAndChangByEmployees
+WITH SCHEMABINDING
+AS
+	SELECT 
+		 "CategoryName"
+		,"ProductName"
+		,"InventoryDate"
+		,"Count"
+		,CONCAT("EmployeeFirstName",' ',"EmployeeLastName") AS "EmployeeName"
+	FROM dbo.Categories AS C
+	JOIN dbo.Products AS P ON P."CategoryID" = C."CategoryID"
+	JOIN dbo.Inventories AS I ON I."ProductID" = P."ProductID"
+	JOIN dbo.Employees AS E ON I."EmployeeID" = E."EmployeeID"
+	WHERE "ProductName" IN ('Chai', 'Chang')
+GO
+--SELECT * FROM vInventoriesForChaiAndChangByEmployees
+
+-- Question 9 (10% pts): How can you create a view to show a list of Employees and the Manager who manages them?
+-- Order the results by the Manager's name!
+CREATE OR ALTER VIEW vEmployeesByManager
+WITH SCHEMABINDING
+AS
+	SELECT TOP 1000000000
+		 CONCAT(M."EmployeeFirstName",' ',M."EmployeeLastName") AS "ManagerName"
+		,CONCAT(E."EmployeeFirstName",' ',E."EmployeeLastName") AS "EmployeeName"
+	FROM dbo.Employees AS E
+	LEFT JOIN dbo.Employees AS M ON E.ManagerID = M.EmployeeID --using LEFT join as the boss won't have a manager
+	ORDER BY "ManagerName", "EmployeeName"
+GO
+--SELECT * FROM vEmployeesByManager
+
+-- Question 10 (20% pts): How can you create one view to show all the data from all four 
+-- BASIC Views? Also show the Employee's Manager Name and order the data by 
+-- Category, Product, InventoryID, and Employee.
+
+CREATE OR ALTER VIEW vInventoriesByProductsByCategoriesByEmployees
+WITH SCHEMABINDING
+AS
+	SELECT TOP 1000000000
+		 C."CategoryID"
+		,"CategoryName"
+		,P."ProductID"
+		,"ProductName"
+		,"UnitPrice"
+		,"InventoryID"
+		,"InventoryDate"
+		,"Count"
+		,E."EmployeeID"
+		,CONCAT(E."EmployeeFirstName",' ',E."EmployeeLastName") AS "EmployeeName"
+		,CONCAT(M."EmployeeFirstName",' ',M."EmployeeLastName") AS "ManagerName"
+	FROM dbo.Categories AS C
+	JOIN dbo.Products AS P ON P."CategoryID" = C."CategoryID"
+	JOIN dbo.Inventories AS I ON I."ProductID" = P."ProductID"
+	JOIN dbo.Employees AS E ON I."EmployeeID" = E."EmployeeID"
+	LEFT JOIN dbo.Employees AS M ON E.ManagerID = M.EmployeeID --using LEFT join as the boss won't have a manager
+	ORDER BY "CategoryName", "ProductName", "InventoryID","EmployeeName"
+GO
+
+-- Test your Views (NOTE: You must change the your view names to match what I have below!)
+Print 'Note: You will get an error until the views are created!'
+Select * From [dbo].[vCategories]
+Select * From [dbo].[vProducts]
+Select * From [dbo].[vInventories]
+Select * From [dbo].[vEmployees]
+
+Select * From [dbo].[vProductsByCategories]
+Select * From [dbo].[vInventoriesByProductsByDates]
+Select * From [dbo].[vInventoriesByEmployeesByDates]
+Select * From [dbo].[vInventoriesByProductsByCategories]
+Select * From [dbo].[vInventoriesByProductsByEmployees]
+Select * From [dbo].[vInventoriesForChaiAndChangByEmployees]
+Select * From [dbo].[vEmployeesByManager]
+Select * From [dbo].[vInventoriesByProductsByCategoriesByEmployees]
+
+/***************************************************************************************/
